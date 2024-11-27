14cb8869af662600ffcd95ef6709dc369d9a30b7
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-11-27 13:29:43 +0100

    Add project files.

---
+++ VirtualPetParProg/Program.cs
@@ -0,0 +1,33 @@
+﻿using VirtualPetParProg;
+
+internal class Program
+{
+    Animal animal1 = new Animal();
+    animal1.Name = 
+}
+
+
+
+
+
+
+
+/*
+  Virtual Pet
+Lag en konsoll-applikasjon hvor man kan generere opp flere forskjellige virtuelle kjæledyr.
+Bruk constructor i klassen, og man skal ikke ha lov til å endre data som navn, age eller noen andre egenskaper utenfra klassen. 
+Lag funksjonalitet som gjør at man kan gi dyret mat, kose med dyret eller sjekke om dyret må på do
+Her er et eksempel på hvordan det kan se ut:
+Hvilket dyr vil du ta vare på? 
+Pikachu
+1. Gi Pikachu mat
+2. Kos med Pikachu
+3. Sjekk om Pikachu må på do
+2
+Pikachu smiler!
+1. Gi Pikachu mat
+2. Kos med Pikachu
+3. Sjekk om Pikachu må på do
+4. Pikachu er mett og fornøyd
+
+ */
