f4d5e8f04c84dbae8d947647d220eda99d612626
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-11-28 13:31:15 +0100

    update

--- VirtualPetParProg/Animal.cs
+++ VirtualPetParProg/Animal.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VirtualPetParProg
+﻿namespace VirtualPetParProg
 {
     internal class Animal
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Race { get; set; }
+        public string Colour { get; set; }
+        public int Petting { get; set; }
+        public int ToiletTime { get; set; }
+        public int Hunger { get; set; }
+
+
+        public Animal(string name, int age, string race, string colour, int petting, int toiletTime, int hunger)
+        {
+            Name = name;
+            Age = age;
+            Race = race;
+            Colour = colour;
+            Petting = petting;
+            ToiletTime = toiletTime;
+            Hunger = hunger;
+        }
+
+
+        public void ChooseAnimal(int optionNumber)
+        {
+            Console.WriteLine($"Choose your pet: \n {optionNumber}: {Name} the {Race}");
+        }
     }
 }
--- VirtualPetParProg/Program.cs
+++ VirtualPetParProg/Program.cs
@@ -1,14 +1,101 @@
-﻿using VirtualPetParProg;
+﻿using System;
 
-internal class Program
+namespace VirtualPetParProg
 {
-    Animal animal1 = new Animal();
-    animal1.Name = 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Animal animal1 = new Animal("Kevin", 5, "Dog", "White", 10, 10, 10);
+            Animal animal2 = new Animal("Frenk", 8, "Cat", "Brown", 10, 10, 10);
+
+
+            animal1.ChooseAnimal(1);
+            animal2.ChooseAnimal(2);
+
+
+            var chosenAnimal = Console.ReadLine();
+
+
+            Animal selectedAnimal = null;
+            switch (chosenAnimal)
+            {
+                case "1":
+                    selectedAnimal = animal1;
+                    break;
+                case "2":
+                    selectedAnimal = animal2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, exiting.");
+                    return;
+            }
+
+
+            ShowAnimalMenu(selectedAnimal);
+        }
+
+
+        public static void ShowAnimalMenu(Animal animal)
+        {
+            Console.WriteLine($"You chose {animal.Name} the {animal.Race}.");
+            Console.WriteLine($"Age: {animal.Age}");
+            Console.WriteLine($"Colour: {animal.Colour}");
+            Console.WriteLine($"Petting: {animal.Petting}");
+            Console.WriteLine($"Toilet Time: {animal.ToiletTime}");
+            Console.WriteLine($"Hunger: {animal.Hunger}");
+
+            Console.WriteLine("1. Feed the animal");
+            Console.WriteLine("2. Cuddle the animal");
+            Console.WriteLine("3. Let the animal go to the toilet");
+
+            int action;
+            action = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (action)
+            {
+                case 1:
+                    Feed(animal);
+                    break;
+                case 2:
+                    Cuddle(animal);
+                    break;
+                case 3:
+                    Toilet(animal);
+                    break;
+                default:
+                    Console.WriteLine("Invalid action.");
+                    break;
+            }
+
+
+            ShowAnimalMenu(animal);
+        }
+
+
+        public static void Feed(Animal animal)
+        {
+            animal.Hunger--;
+            Console.WriteLine($"{animal.Name} has been fed. Hunger is now {animal.Hunger}.");
+        }
+
+
+        public static void Cuddle(Animal animal)
+        {
+            animal.Petting++;
+            Console.WriteLine($"{animal.Name} is cuddled. Petting is now {animal.Petting}.");
+        }
+
+
+        public static void Toilet(Animal animal)
+        {
+            animal.ToiletTime--;
+            Console.WriteLine($"{animal.Name} went to the toilet. Toilet time is now {animal.ToiletTime}.");
+        }
+    }
 }
-
-
-
-
 
 
 
